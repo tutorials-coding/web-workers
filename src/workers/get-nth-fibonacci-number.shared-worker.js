da66fce265be271c1e24e70b778a3e47f3e4e7af
Vadim <korobeinikovv@mail.ru>
2020-11-18 10:34:17 +0300

    Add shared worker

---
+++ src/workers/get-nth-fibonacci-number.shared-worker.js
@@ -0,0 +1,20 @@
+import { getNthFobonacciNumber } from "../helpers/get-nth-fibonacci-number";
+
+let connectedTimes = 0;
+
+// need to subscribe to connect
+self.onconnect = function (e) {
+  connectedTimes++;
+  const port = e.ports[0];
+  port.onmessage = function (e) {
+    const num = e.data;
+    if (Number.isInteger(parseInt(num))) {
+      const result = getNthFobonacciNumber(num);
+      port.postMessage(result);
+    } else {
+      port.postMessage("Is not a number");
+      throw new Error("Is not a number");
+    }
+  };
+  port.start(); // not necessary as onmessage is used
+};
